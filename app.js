770232419ec9a6c2977480c57b0d6c0452d92451
georgeadono <82292588+georgeadono@users.noreply.github.com>
2023-01-19 00:01:01 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 
 
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
